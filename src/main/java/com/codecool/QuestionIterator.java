1c804d84f11eedd80c99ac8d24fb5d045e024053
dnorbert4000 <dnorbert@gmail.com>
2018-02-06 11:52:35 +0100

    question iterator function created

--- src/main/java/com/codecool/QuestionIterator.java
+++ src/main/java/com/codecool/QuestionIterator.java
@@ -1,6 +1,39 @@
 package com.codecool;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
-public interface QuestionIterator implements Iterator<Question> {
+public class QuestionIterator implements Iterator<Question> {
+
+    private Question[] arrayList;
+    private int currentSize;
+
+    public QuestionIterator(Question[] newArray) {
+        this.arrayList = newArray;
+        this.currentSize = arrayList.length;
+}
+
+    @Override
+    public Iterator<Question> iterator() {
+        Iterator<Question> it = new Iterator<Question>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < currentSize && arrayList[currentIndex] != null;
+            }
+
+            @Override
+            public Question next() {
+                return arrayList[currentIndex++];
+            }
+
+            /*@Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }*/
+        };
+        return it;
+
 }
