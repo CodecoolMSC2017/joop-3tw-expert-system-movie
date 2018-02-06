c930b94713b0915a93ad73c59661e55c93b6d986
krisztianKollar <kollarkr@gmail.com>
2018-02-06 11:48:28 +0100

    xmlparser in process

--- src/main/java/com/codecool/XMLParser.java
+++ src/main/java/com/codecool/XMLParser.java
@@ -1,6 +1,17 @@
 package com.codecool;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public abstract class XMLParser {
+
+    protected Document document;
 
     public XMLParser() {
 
@@ -8,5 +19,29 @@ public abstract class XMLParser {
 
     public void loadXmlDocument(String xmlPath) {
 
+        try {
+
+            File fXmlFile = new File(String xmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            document = doc;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private Element[] getElements(String tagName) {
+        NodeList nodeList = document.getElementsByTagName(tagName);
+        Element[] elements = new Element[nodeList.getLength()];
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            elements[i] = (Element)node;
+        }
+        return elements;
     }
 }
