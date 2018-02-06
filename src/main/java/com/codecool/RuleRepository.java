45f86db44066ee590d383a3ee3774c696e333de9
dnorbert4000 <dnorbert@gmail.com>
2018-02-06 14:08:55 +0100

    new methods

--- src/main/java/com/codecool/RuleRepository.java
+++ src/main/java/com/codecool/RuleRepository.java
@@ -1,17 +1,22 @@
 package com.codecool;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class RuleRepository {
+    private Iterator<Question> iterator;
+    private List<Question> list = new ArrayList<Question>();
 
     public RuleRepository() {
     }
 
     public void addQuestion(Question question) {
-
+        List.add(question);
     }
 
     public Iterator<Question> getIterator() {
-
+        iterator = new QuestionIterator(list);
+        return iterator;
     }
 }
