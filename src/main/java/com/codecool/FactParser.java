eb96972fdfa9b9362582b3436f55844ddb31b07e
HajnalFeri <kalacsf1@gmail.com>
2018-02-06 14:06:22 +0100

    Fact xml read

--- src/main/java/com/codecool/FactParser.java
+++ src/main/java/com/codecool/FactParser.java
@@ -1,12 +1,40 @@
 package com.codecool;
 
-public class FactParser {
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-    public FactParser() {
-    }
+public class FactParser extends XMLParser{
 
-    public FactRepository getFactRepository() {
+    String id, description;
+    boolean value;
+    Fact fact;
 
+    public FactRepository getFactRepository() throws Exception {
+        loadXmlDocument("Facts.xml");
+        FactRepository factRepository = new FactRepository();
+        for (Element ruleElement : getElements("Fact")) {
+
+            Node nNode = ruleElement;
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                id = eElement.getAttribute("id");
+                description = eElement.getElementsByTagName("Description").item(0).getAttributes().getNamedItem("value").getNodeValue();
+                fact = new Fact(id, description);
+                NodeList evals = eElement.getElementsByTagName("Eval");
+                for (int i = 0; i < evals.getLength(); i++) {
+                    Node mNode = evals.item(i);
+                    if(mNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element eval = (Element)mNode;
+                        String evalId = eval.getAttribute("id");
+                        value = Boolean.parseBoolean(eval.getTextContent().trim());
+                        fact.setFactValueById(evalId, value);
+                    }
+                }
+                factRepository.addFact(fact);
+            }
+        }
+        return factRepository;
     }
 }
 
--- src/main/java/com/codecool/FactRepository.java
+++ src/main/java/com/codecool/FactRepository.java
@@ -1,18 +1,25 @@
 package com.codecool;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class FactRepository {
 
-    public FactRepository() {
 
-    }
+    private List<Fact> facts = new ArrayList<>();
+
 
     public void addFact(Fact fact) {
+        facts.add(fact);
+    }
 
+    public List<Fact> getFactList() {
+        return facts;
     }
 
     public Iterator<Fact> getIterator() {
 
+        return null;
     }
 }
