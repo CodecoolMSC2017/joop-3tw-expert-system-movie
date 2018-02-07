b1b0fd7eff8846bc3aef04d5812a0cba5d5d3e8a
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-02-07 15:55:29 +0100

    Answer object now properly filled up and passed to Rule object

--- src/main/java/com/codecool/RuleParser.java
+++ src/main/java/com/codecool/RuleParser.java
@@ -9,14 +9,21 @@ public class RuleParser extends XMLParser {
     String id, q;
     boolean value;
     Question question;
-    Answer answer;
+    Answer answer = new Answer();
+    Value singleValue;
+    Value multipleValue;
 
     public RuleRepository getRuleRepository() {
+
+        String selectionValue;
+        boolean selectionBool = true;
+
         loadXmlDocument("data/Rules.xml");
         RuleRepository ruleRepository = new RuleRepository();
 
         for (Element ruleElement : getElements("Rule")) {
             id = ruleElement.getAttribute("id");
+
             Node questionElement = ruleElement.getElementsByTagName("Question").item(0);
             q = questionElement.getTextContent();
 
@@ -25,20 +32,25 @@ public class RuleParser extends XMLParser {
 
             for (int i = 0; i < selectionNodes.getLength(); i++) {
                 Element selectionElement = (Element) selectionNodes.item(i);
-                String selectionValue = selectionElement.getAttribute("value");
+                selectionValue = selectionElement.getAttribute("value");
+                if (selectionValue.equals("true")){
+                    selectionBool = true;
+                }
+                else if (selectionValue.equals("false")){
+                    selectionBool = false;
+                }
 
                 NodeList singleValueNodes = selectionElement.getElementsByTagName("SingleValue");
                 NodeList multipleValueNodes = selectionElement.getElementsByTagName("MultipleValue");
 
                 if (singleValueNodes.getLength() == 0) {
                     Element multipleValueElement = (Element) multipleValueNodes.item(0);
-                    System.out.println(multipleValueElement.getTagName());
                 } else {
                     Element singleValueElement = (Element) singleValueNodes.item(0);
-                    System.out.println(singleValueElement.getAttribute("value"));
+                    String s = singleValueElement.getAttribute("value");
+                    answer.addValue(new SingleValue(s, selectionBool));
                 }
             }
-
             this.question = new Question(id, q, answer);
             ruleRepository.addRule(this.question);
         }
