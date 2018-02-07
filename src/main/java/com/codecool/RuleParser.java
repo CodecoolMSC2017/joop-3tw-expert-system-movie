797cfb88da078395fee890c0d5cd624f4ed77bea
krisztianKollar <kollarkr@gmail.com>
2018-02-07 10:28:16 +0100

    ruleparser in process

--- src/main/java/com/codecool/RuleParser.java
+++ src/main/java/com/codecool/RuleParser.java
@@ -15,25 +15,27 @@ public class RuleParser extends XMLParser {
         loadXmlDocument("data/Rules.xml");
         RuleRepository ruleRepository = new RuleRepository();
         for (Element ruleElement : getElements("Rule")) {
+            id = ruleElement.getAttribute("id");
+            Node questionElement = ruleElement.getElementsByTagName("Question").item(0);
+            q = questionElement.getTextContent().getNamedItem("value").getNodeValue();
+            this.question = new Question(id, q, answer);
+            Element answerElement = (Element) ruleElement.getElementsByTagName("Answer").item(0);
+            NodeList selectionNodes = answerElement.getElementsByTagName("Selection");
+            for (int i = 0; i < selectionNodes.getLength(); i++) {
+                Element selectionElement = (Element) selectionNodes.item(i);
 
-            Node nNode = ruleElement;
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
-                id = eElement.getAttribute("id");
-                q = eElement.getElementsByTagName("Question").item(0).getAttributes().getNamedItem("value").getNodeValue();
-                question = new Question(id, q, answer);
-                NodeList answers = eElement.getElementsByTagName("Answer");
-                for (int i = 0; i < answers.getLength(); i++) {
-                    Node mNode = answers.item(i);
-                    if(mNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element answer = (Element)mNode;
-                        String value = answer.getAttribute("id");
-                        /*value = Boolean.parseBoolean(eval.getTextContent().trim());
-                        fact.setFactValueById(evalId, value);*/
-                    }
+                NodeList singleValueNodes = selectionElement.getElementsByTagName("SingleValue");
+                NodeList multipleValueNodes = selectionElement.getElementsByTagName("MultipleValue");
+
+                if (singleValueNodes.getLength() == 0) {
+                    Element multipleValueElement = (Element) multipleValueNodes.item(0);
+                } else {
+                    Element singleValueElement = (Element) singleValueNodes.item(0);
                 }
-                ruleRepository.addRule(question);
             }
+            /*value = Boolean.parseBoolean(eval.getTextContent().trim());
+            fact.setFactValueById(evalId, value);*/
+            ruleRepository.addRule(this.question);
         }
         return ruleRepository;
     }
