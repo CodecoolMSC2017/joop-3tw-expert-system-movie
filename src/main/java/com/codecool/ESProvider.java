7abdbaff84fd16317afa6dfb83ccbd5dbb13a1ec
krisztianKollar <kollarkr@gmail.com>
2018-02-08 13:57:32 +0100

    just some addition to the print code in ESProvider

--- src/main/java/com/codecool/ESProvider.java
+++ src/main/java/com/codecool/ESProvider.java
@@ -81,6 +81,6 @@ public class ESProvider {
             factString = "";
             qaString = "";
         }
-        System.out.println(temp);
+        System.out.println("What you should try: " + temp);
     }
 }
