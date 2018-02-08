bc67a07f895aa3c2c297f7a5de99e3dd5d10ba00
Zsolt Gajdacs <gajdacs.zsolt@gmail.com>
2018-02-08 13:56:00 +0100

    Answer and Question tests complete

--- src/test/java/com/codecool/AnswerTest.java
+++ src/test/java/com/codecool/AnswerTest.java
@@ -1,5 +1,37 @@
+package com.codecool;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnswerTest {
 
+    Answer answer;
+
+    @BeforeEach
+    void setUp() {
+        answer = new Answer();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void evaluateAnswerByInput() {
+        assertTrue(answer.evaluateAnswerByInput("yes"));
+        assertFalse(answer.evaluateAnswerByInput("Yes"));
+        assertFalse(answer.evaluateAnswerByInput("no"));
+        assertFalse(answer.evaluateAnswerByInput(""));
+    }
+
+    @Test
+    void addValue() {
+        answer.addValue(new SingleValue("test", true));
+        assertEquals("test", ((SingleValue)(answer.valueList.get(0))).getParam());
+        assertTrue(answer.valueList.get(0).getSelectionType());
+        assertEquals(1, answer.valueList.size());
+    }
 }
--- src/test/java/com/codecool/QuestionTest.java
+++ src/test/java/com/codecool/QuestionTest.java
@@ -1,5 +1,42 @@
+package com.codecool;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionTest {
 
+    Question question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question("test", "What's a test?", new Answer());
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+        assertEquals("test", question.getId());
+    }
+
+    @Test
+    void getQuestion() {
+        assertEquals("What's a test?", question.getQuestion());
+    }
+
+    @Test
+    void getAnswer() {
+        assertEquals(0, question.getAnswer().valueList.size());
+    }
+
+    @Test
+    void getEvaluatedAnswer() {
+        assertTrue(question.getEvaluatedAnswer("yes"));
+        assertFalse(question.getEvaluatedAnswer("no"));
+    }
 }
